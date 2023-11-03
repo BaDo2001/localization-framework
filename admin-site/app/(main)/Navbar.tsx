77d8f1cc502a2bcb907b8ece2a1b0c6496330c7e
Balint Dolla <balint@apexlab.io>
2023-11-03 23:05:02 +0100

    Add back to home button to navbar

--- admin-site/app/(main)/Navbar.tsx
+++ admin-site/app/(main)/Navbar.tsx
@@ -1,13 +1,30 @@
-import { SignOutButton } from "@clerk/nextjs";
+"use client";
 
-const Navbar = async () => (
-  <nav className="flex flex-col items-center bg-base-100 py-8 px-4 w-80">
-    <SignOutButton>
-      <button className="btn btn-outline w-2/3" type="button">
-        Sign out
-      </button>
-    </SignOutButton>
-  </nav>
-);
+import { FaChevronLeft } from "react-icons/fa";
+
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex flex-col items-center bg-base-100 py-8 px-4 w-80">
+      <SignOutButton>
+        <button className="btn btn-outline w-2/3" type="button">
+          Sign out
+        </button>
+      </SignOutButton>
+
+      {pathname !== "/" && (
+        <Link href="/" className="btn [&>svg]:w-6 [&>svg]:h-6 mt-auto">
+          <FaChevronLeft />
+          Back to home
+        </Link>
+      )}
+    </nav>
+  );
+};
 
 export default Navbar;
