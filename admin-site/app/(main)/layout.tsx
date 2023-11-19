1e76727fddcff9b98c28280abcbb53bb722fa8fe
tothpeterdev <tothpeti2000@edu.bme.hu>
2023-11-19 20:56:10 +0100

    Navbar file name modified

--- admin-site/app/(main)/layout.tsx
+++ admin-site/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import type { FC, PropsWithChildren } from "react";
 
-import Navbar from "./navbar/Navbar";
+import Navbar from "./navbar";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => (
   <main className="flex h-full items-stretch">
